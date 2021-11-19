1228d18c6d944da36cd718fc38a996c97d03c907
wb-hbc831417 <wb-hbc831417@alibaba-inc.com>
2021-11-19 17:43:03 +0800

    feat: insert antd form snippet

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,70 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "moyu" is now active!');
+const formItems = ['input', 'select', 'empty']
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('moyu.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from moyu-template!');
+const selectItem: () => Promise<string[] | []> = async () => {
+	const value = await vscode.window.showQuickPick([...formItems, 'end'])
+	if (!value) return []
+	if (value === 'end') {
+		return []
+	} else {
+		vscode.window.showInformationMessage(`增加${value}组件`)
+		return [value, ...await selectItem()];
+	}
+}
+
+const itemMap: Record<string, string> = {
+	'input': `<Form.Item \n\tname="input" \n\tlabel="input"\n>\n\t<Input />\n</Form.Item>`,
+	'select': `<Form.Item \n\tname="select" \n\tlabel="select"\n>\n\t<Select />\n</Form.Item>`,
+	'empty': `<Form.Item \n\tname="empty" \n\tlabel="empty">\n<></>\n</Form.Item>`,
+}
+
+const generateFormItemCode: (itemList: string[] | []) => vscode.SnippetString = (itemList) => {
+	const str = itemList.map(i => itemMap[i]).join('')
+	return new vscode.SnippetString(str);
+}
+
+export function activate(context: vscode.ExtensionContext) {
+
+	let addForm = vscode.commands.registerTextEditorCommand('moyu.add a antd form', async (edit) => {
+		const first20LineRange = new vscode.Range(new vscode.Position(0, 0), new vscode.Position(21, 0))
+		const first20Text = edit.document.getText(first20LineRange)
+		// TODO 考虑双引号
+		const theLineNumber = first20Text.split('\n').findIndex(i => i.includes("from 'antd'")) + 1
+		const theLine = first20Text.split('\n').find(i => i.includes("from 'antd'"))
+		console.log('%ctheLine', 'background-color: darkorange', theLine);
+		console.log('%cfirst20Text', 'background-color: darkorange', first20Text);
+		// const itemList = await selectItem();
+
+		// 创建文件夹
+		// if (vscode.workspace.workspaceFolders) {
+		// 	const rootPathURI = vscode.workspace.workspaceFolders[0].uri
+		// 	const newPath = vscode.Uri.joinPath(rootPathURI, '/test');
+		// 	vscode.workspace.fs.createDirectory(newPath)
+		// }
+
+		/**
+		 * insert code
+		 */
+
+		// New editor
+		// const editor = vscode.window.activeTextEditor
+
+
+		// Create a new snippet
+		// const snippet = generateFormItemCode(itemList);
+
+		// insert text
+		// editor?.insertSnippet(snippet);
 	});
 
-	context.subscriptions.push(disposable);
+	const formWith = vscode.commands.registerTextEditorCommand('moyu.form with', () => {
+		console.log('adf')
+	})
+
+	context.subscriptions.push(addForm);
+	context.subscriptions.push(formWith);
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
